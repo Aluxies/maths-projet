eba65dd07e55361b7dc1a17b73190f9733911adc
vinz <viencentmeessen@gmail.com>
2025-12-12 16:39:42 +0100

    update

--- main.py
+++ main.py
@@ -40,45 +40,36 @@ def generate_plot_random_walk(errors, k):
 	plt.ylabel("Erreur moyenne ε(k)")
 	plt.title("Convergence de la marche aléatoire vers PageRank personnalisé")
 	plt.ylim(bottom=0, top=1.1)
-	
+
 	plt.grid(True)
 	plt.show()
 
 if __name__ == "__main__":
 	alpha = 0.9
 	v = read_personnalisation("csv/VecteurPersonnalisation_Groupe19.csv")
-
-
-
 	generate_matrix_csv()
 	A = read_csv_matrix("csv/matrix_adj.csv")
 
-	print("Contenu de la matrice d’adjacence: \n", A)
-	print("Contenu du vecteur : \n", v)
+	print(f"Contenu de la matrice d’adjacence: \n{A}\n")
+	print(f"Contenu du vecteur : \n{v}\n\n\n")
 
 
-
-
-	print("pageRankLinear: \n")
+	print("> pageRankLinear: \n")
 	x1 = pageRankLinear(A, alpha, v)
-	print(f"x: {x1}\n")
-	print("Somme des scores (=1?) :", x1.sum())
-
-	print("pageRankPower: \n")
-	x2 = pageRankPower(A, alpha, v)
-
-	print(f"x: {x2}\n")
-
-	print("pageRankRandomWalk: \n")
-	x3, errors, k = randomWalk(A, alpha, v)
-
-	print("")
-	
-	print(f"x: {x3}\n")
-
-	generate_plot_random_walk(errors, k)
-
+	print(f"{x1}\n")
+	print(f"Somme des scores (=1?) : {x1.sum()}\n",)
 	# On remarque que que niveau d'importance du plus bas au plus haut est:
 	# H, G, A, C, F, D, E, B, J, I
 
 
+	print("> pageRankPower: \n")
+	x2 = pageRankPower(A, alpha, v)
+	print(f"{x2}\n")
+
+
+	print("> pageRankRandomWalk: \n")
+	x3, errors, k = randomWalk(A, alpha, v)
+	print(f"x: {x3}\n")
+
+
+	generate_plot_random_walk(errors, k)
--- pr_funcs.py
+++ pr_funcs.py
@@ -1,125 +1,6 @@
 import numpy as np
 
-def pageRankLinear(A: np.matrix, alpha: float, v: np.array) -> np.array:
-
-	n = A.shape[0] # taille vecteur
-
-	P = probabilityTransitionMatrix(A, n)
-
-	# Système linéaire : (I - alpha*P) x = (1 - alpha) * v
-	I = np.eye(n) # (matrice identité)
-	x = np.linalg.solve(I - alpha * P, (1 - alpha) * v) # resolution système linéaire
-	x /= x.sum()  # Normalisation final
-	return x
-
-
-def pageRankPower(A: np.matrix, alpha: float, v: np.array) -> np.array:
-
-	print(A)
-	# v -> personalization vector
-
-	# max number of iterations
-	max_iter = 150
-
-	# variation tolerance
-	var_tol = 1e-9
-
-	# matrix side size
-	n = A.shape[0]
-
-	# probability transition matrix
-	P = probabilityTransitionMatrix(A, n)
-
-	print(P)
-
-	# calculating transposition of v
-	vt = v[None, :]
-
-	# identity vector
-	e = np.ones((n, 1))
-
-	# google = alpha * P + (1 - alpha) e*v^T
-	G = alpha * P.T + (1 - alpha) * (e @ vt)
-
-	# init : indegree per column
-	pi = A.sum(axis=0)
-	# normalise the vector
-	pi = pi / pi.sum()
-
-	# iterating by calculating the Google matrix
-	for i in range(max_iter):
-		pi_new = pi @ G
-
-		# if variation is under the variation tolerance number we stop iterations
-		if np.linalg.norm(pi_new - pi, 1) < var_tol:
-			break
-
-		# printing the 3 first iterations
-		if i < 3:
-			print(pi_new)
-		
-		pi = pi_new
-	
-	return pi
-
-
-def randomWalk(A: np.matrix, alpha: float, v: np.array, nsteps: int = 20000, start_node: int = 0) -> np.array:
-
-	# random number generator
-	rng = np.random.default_rng(0)
-
-	# matrix side size
-	n = A.shape[0]
-
-	# probability transition matrix
-	P = probabilityTransitionMatrix(A, n)
-
-	# vector of visits count for each node
-	visits = np.zeros(n, dtype=float)
-
-	# calculating transposition of v
-	vt = v[None, :]
-
-	# identity vector
-	e = np.ones((n, 1))
-
-	# google = alpha * P + (1 - alpha) e*v^T
-	G = alpha * P.T + (1 - alpha) * (e @ vt)
-
-	# current node index
-	current = start_node
-
-	page_rank_linear = pageRankLinear(A, alpha, v)
-
-	logs_every = 500
-
-	errors = []
-	k = []
-
-	# iterating by calculating the Google matrix
-	for i in range(nsteps+1):
-		# teleportation based on personnalisation vector
-		current = rng.choice(n, p=G[current])
-		visits[current] += 1.0
-		
-		if i % logs_every == 0:
-			# Calculating the mean error for this iteration
-			mean_error = 1 / (1 + np.abs(page_rank_linear - pageRankRandomWalk(visits)).mean())
-			errors.append(mean_error)
-			k.append(i)
-	
-	return pageRankRandomWalk(visits), errors, k
-
-def pageRankRandomWalk(visits: np.array) -> np.array:
-	if visits.sum() > 0:
-		distance = visits / visits.sum()
-	else:
-		distance = visits
-	
-	return distance
-
 def probabilityTransitionMatrix(A: np.matrix, n: int):
-
 	col_sums = A.sum(axis=0) # somme des columns
 	#print("col_sums",col_sums)
 
@@ -134,3 +15,81 @@ def probabilityTransitionMatrix(A: np.matrix, n: int):
 
 	#print("p",P)
 	return P
+
+def pageRankLinear(A: np.matrix, alpha: float, v: np.array) -> np.array:
+	n = A.shape[0] # taille vecteur
+	P = probabilityTransitionMatrix(A, n)
+
+	# Système linéaire : (I - alpha*P) x = (1 - alpha) * v
+	I = np.eye(n) # (matrice identité)
+	x = np.linalg.solve(I - alpha * P, (1 - alpha) * v) # resolution système linéaire
+	x /= x.sum()  # Normalisation final
+	return x
+
+
+def pageRankPower(A: np.matrix, alpha: float, v: np.array) -> np.array:
+	max_iter = 150 # max number of iterations
+	var_tol = 1e-9 # variation tolerance
+
+	n = A.shape[0] # matrix side size
+	P = probabilityTransitionMatrix(A, n) # probability transition matrix
+
+	vt = v[None, :] # calculating transposition of v
+	e = np.ones((n, 1)) # identity vector
+	G = alpha * P.T + (1 - alpha) * (e @ vt) # google = alpha * P + (1 - alpha) e*v^T
+
+	pi = A.sum(axis=0) # init : indegree per column
+	pi = pi / pi.sum() # normalise the vector
+
+	for i in range(max_iter): # iterating by calculating the Google matrix
+		pi_new = pi @ G
+		# if variation is under the variation tolerance number we stop iterations
+		if np.linalg.norm(pi_new - pi, 1) < var_tol:
+			break
+		# printing the 3 first iterations
+		if i < 3:
+			print(pi_new)
+		pi = pi_new
+	return pi
+
+
+def randomWalk(A: np.matrix, alpha: float, v: np.array, nsteps: int = 20000, start_node: int = 0) -> np.array:
+	rng = np.random.default_rng(0) # random number generator
+
+	n = A.shape[0] # matrix side size
+	P = probabilityTransitionMatrix(A, n) # probability transition matrix
+
+	visits = np.zeros(n, dtype=float) # vector of visits count for each node
+
+	vt = v[None, :] # calculating transposition of v
+	e = np.ones((n, 1)) # identity vector
+	G = alpha * P.T + (1 - alpha) * (e @ vt) # google = alpha * P + (1 - alpha) e*v^T
+
+	current = start_node # current node index
+
+	page_rank_linear = pageRankLinear(A, alpha, v)
+
+	logs_every = 500
+	errors = []
+	k = []
+	for i in range(nsteps+1): # iterating by calculating the Google matrix
+		# teleportation based on personnalisation vector
+		current = rng.choice(n, p=G[current])
+		visits[current] += 1.0
+
+		if i % logs_every == 0:
+			# Calculating the mean error for this iteration
+			mean_error = 1 / (1 + np.abs(page_rank_linear - pageRankRandomWalk(visits)).mean())
+			errors.append(mean_error)
+			k.append(i)
+
+	return pageRankRandomWalk(visits), errors, k
+
+
+def pageRankRandomWalk(visits: np.array) -> np.array:
+	if visits.sum() > 0:
+		distance = visits / visits.sum()
+	else:
+		distance = visits
+
+	return distance
